f65fc3e9a81bf68ce26615a40bca373ab0883baf
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2025-08-04 09:47:23 +0200

    test: add new tests

--- tests/test_response.py
+++ tests/test_response.py
@@ -1,4 +1,6 @@
-from juracoffeemachine import JuraProtocol, JuraCommand, CS, HZ
+import pytest
+
+from juracoffeemachine import JuraProtocol, JuraCommand, CS, HZ, IC
 from test_jura import ValidSerial
 
 
@@ -19,6 +21,7 @@ def test_valid_hz():
     p = JuraProtocol(t, unexpected_msg_callback=lambda c: callback())
     hz = p.get_and_parse_message(JuraCommand.HZ)
     assert isinstance(hz, HZ)
+    assert t.read_index == len(t.read_buffer)
 
     assert not hz.is_sleeping
     assert not hz.is_bowl_moving
@@ -31,6 +34,54 @@ def test_valid_hz():
     assert not hz.is_draining_tray_full
 
 
+def test_wrong_static_hz():
+    t = ValidSerial()
+    t.read_buffer = encode_str("hz:00010110000000,0288,00ED,0107,03E8,0000,0,0017,000100,12")
+    callback_called = [False]
+
+    def callback(c):
+        callback_called[0] = True
+
+    p = JuraProtocol(t, unexpected_msg_callback=lambda c: callback(c))
+    hz = p.get_and_parse_message(JuraCommand.HZ)
+    assert isinstance(hz, HZ)
+    assert callback_called[0]
+    assert t.read_index == len(t.read_buffer)
+
+    assert not hz.is_sleeping
+    assert not hz.is_bowl_moving
+    assert hz.bowl_pos == 237
+    assert hz.water_vol == 263
+    assert hz.heater == 1000
+    assert hz.is_water_tank_present
+    assert not hz.is_coffee_waste_full
+    assert hz.is_draining_tray_present
+    assert not hz.is_draining_tray_full
+
+
+@pytest.mark.parametrize(
+    "hz_msg",
+    [
+        "hz:01010110000000,0288,00ED,0107,03E8,0000,0,",
+        "hz:00010110000000,0288,00ED,0107,03E8,000,0,0017,000100,12",
+        "hz:010101100000000288,00ED,0107,03E8,0000,0,0017,000100,12"
+    ],
+)
+def test_format_hz(hz_msg):
+    t = ValidSerial()
+    t.read_buffer = encode_str(hz_msg)
+    callback_called = [False]
+
+    def callback(c):
+        callback_called[0] = True
+
+    p = JuraProtocol(t, unexpected_msg_callback=lambda c: callback(c))
+    hz = p.get_and_parse_message(JuraCommand.HZ)
+    assert hz is None
+    assert callback_called[0]
+    assert t.read_index == len(t.read_buffer)
+
+
 def test_valid_cs():
     t = ValidSerial()
     t.read_buffer = encode_str("cs:03770000000ED000000000000006000011C00000000")
@@ -41,7 +92,23 @@ def test_valid_cs():
     p = JuraProtocol(t, unexpected_msg_callback=lambda c: callback())
     cs = p.get_and_parse_message(JuraCommand.CS)
     assert isinstance(cs, CS)
+    assert t.read_index == len(t.read_buffer)
 
     assert cs.bowl_pos_2 == 237
     assert cs.water_vol == 284
     assert cs.heater == 887
+
+
+def test_valid_ic():
+    t = ValidSerial()
+    t.read_buffer = encode_str("ic:1706")
+
+    def callback():
+        assert False
+
+    p = JuraProtocol(t, unexpected_msg_callback=lambda c: callback())
+    ic = p.get_and_parse_message(JuraCommand.IC)
+    assert isinstance(ic, IC)
+    assert t.read_index == len(t.read_buffer)
+
+    assert ic.unknown_m == "1706"
