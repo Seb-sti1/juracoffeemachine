e9ffeabeb666c59ac4b9ff37320f85de573823ec
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2025-11-27 08:49:43 +0100

    Revert "fix: missing await and readd tests"
    This reverts commit 37975f75

--- tests/test_coffee_machine.py
+++ tests/test_coffee_machine.py
@@ -5,33 +5,7 @@ from tests.test_jura import ValidSerial
 from tests.test_response import encode_str
 
 
-def init_seq_coffee_brew(t: ValidSerial) -> list[int]:
-    write_buffer = []
-    # write_buffer += encode_str("TY:")
-    # t.read_buffer = encode_str("ty:EF532M V02.03")
-    # write_buffer += encode_str("TL:")
-    # t.read_buffer += encode_str("tl:BL_RL78 V01.31")
-    write_buffer += encode_str("TY:")
-    t.read_buffer += encode_str("ty:EF532M V02.03")
-    write_buffer += encode_str("RE:00D6")
-    t.read_buffer += encode_str("re:0031")
-    write_buffer += encode_str("RE:013C")
-    t.read_buffer += encode_str("re:0014")
-    return write_buffer
-
-
-def trailing_seq_coffee_brew(t: ValidSerial) -> list[int]:
-    write_buffer = []
-    write_buffer += encode_str(JuraCommand.BUTTON_4)
-    t.read_buffer += encode_str("ok:")
-    write_buffer += encode_str("CS:")
-    t.read_buffer += encode_str("cs:03770000000ED000000000000006000011C00000000")
-    write_buffer += encode_str("CS:")
-    t.read_buffer += encode_str("cs:03770000000ED000000000000006000011C00000000")
-    write_buffer += encode_str("CS:")
-    t.read_buffer += encode_str("cs:03770000000ED000000000000006000011C00000000")
-    return write_buffer
-
+# TODO repare tests here
 
 def test_init_valid():
     t = ValidSerial()
@@ -51,91 +25,108 @@ def test_init_valid():
     assert t.write_buffer == write_buffer
 
 
-@pytest.mark.asyncio
-async def test_brew_no_mods_valid():
+@pytest.mark.skip(reason="Requires modification to account for cs monitoring")
+def test_brew_no_mods_valid():
     t = ValidSerial()
-    write_buffer = []
-    # write_buffer = encode_str("TY:")
-    # t.read_buffer = encode_str("ty:EF532M V02.03")
-    # write_buffer += encode_str("TL:")
-    # t.read_buffer += encode_str("tl:BL_RL78 V01.31")
+    write_buffer = encode_str("TY:")
+    t.read_buffer = encode_str("ty:EF532M V02.03")
+    write_buffer += encode_str("TL:")
+    t.read_buffer += encode_str("tl:BL_RL78 V01.31")
     write_buffer += encode_str("TY:")
     t.read_buffer += encode_str("ty:EF532M V02.03")
     write_buffer += encode_str("RE:00D6")
     t.read_buffer += encode_str("re:0031")
     write_buffer += encode_str("RE:013C")
     t.read_buffer += encode_str("re:0014")
-    write_buffer += trailing_seq_coffee_brew(t)
+    write_buffer += encode_str(JuraCommand.BUTTON_4)
+    t.read_buffer += encode_str("ok:")
 
     def callback():
         assert False
 
     p = JuraProtocol(t, unexpected_msg_callback=lambda c: callback())
     m = CoffeeMaker(p)
-    await m.brew_coffee(CoffeeMaker.coffee_bean_param[1],
-                        CoffeeMaker.water_volume_param[1], lambda v: None)
+    m.brew_coffee(CoffeeMaker.coffee_bean_param[1],
+                  CoffeeMaker.water_volume_param[1])
 
     assert t.read_index == len(t.read_buffer)
     assert t.write_buffer == write_buffer
 
 
-@pytest.mark.asyncio
-async def test_brew_more_coffee_valid():
+def init_seq_coffee_brew(t: ValidSerial) -> list[int]:
+    write_buffer = encode_str("TY:")
+    t.read_buffer = encode_str("ty:EF532M V02.03")
+    write_buffer += encode_str("TL:")
+    t.read_buffer += encode_str("tl:BL_RL78 V01.31")
+    write_buffer += encode_str("TY:")
+    t.read_buffer += encode_str("ty:EF532M V02.03")
+    write_buffer += encode_str("RE:00D6")
+    t.read_buffer += encode_str("re:0031")
+    write_buffer += encode_str("RE:013C")
+    t.read_buffer += encode_str("re:0014")
+    return write_buffer
+
+
+@pytest.mark.skip(reason="Requires modification to account for cs monitoring")
+def test_brew_more_coffee_valid():
     t = ValidSerial()
     write_buffer = init_seq_coffee_brew(t)
     write_buffer += encode_str("WE:00D6,0051")
     t.read_buffer += encode_str("ok:")
-    write_buffer += trailing_seq_coffee_brew(t)
+    write_buffer += encode_str(JuraCommand.BUTTON_4)
+    t.read_buffer += encode_str("ok:")
 
     def callback():
         assert False
 
     p = JuraProtocol(t, unexpected_msg_callback=lambda c: callback())
     m = CoffeeMaker(p)
-    await m.brew_coffee(CoffeeMaker.coffee_bean_param[1] + 2,
-                        CoffeeMaker.water_volume_param[1], lambda v: None)
+    m.brew_coffee(CoffeeMaker.coffee_bean_param[1] + 2,
+                  CoffeeMaker.water_volume_param[1])
 
     assert t.read_index == len(t.read_buffer)
     assert t.write_buffer == write_buffer
 
 
-@pytest.mark.asyncio
-async def test_brew_less_water_valid():
+@pytest.mark.skip(reason="Requires modification to account for cs monitoring")
+def test_brew_less_water_valid():
     t = ValidSerial()
     write_buffer = init_seq_coffee_brew(t)
     write_buffer += encode_str("WE:013C,0016")
     t.read_buffer += encode_str("ok:")
-    write_buffer += trailing_seq_coffee_brew(t)
+    write_buffer += encode_str(JuraCommand.BUTTON_4)
+    t.read_buffer += encode_str("ok:")
 
     def callback():
         assert False
 
     p = JuraProtocol(t, unexpected_msg_callback=lambda c: callback())
     m = CoffeeMaker(p)
-    await m.brew_coffee(CoffeeMaker.coffee_bean_param[1],
-                        CoffeeMaker.water_volume_param[1] + CoffeeMaker.water_volume_param[3] * 2, lambda v: None)
+    m.brew_coffee(CoffeeMaker.coffee_bean_param[1],
+                  CoffeeMaker.water_volume_param[1] + CoffeeMaker.water_volume_param[3] * 2)
 
     assert t.read_index == len(t.read_buffer)
     assert t.write_buffer == write_buffer
 
 
-@pytest.mark.asyncio
-async def test_brew_less_coffee_and_water_valid():
+@pytest.mark.skip(reason="Requires modification to account for cs monitoring")
+def test_brew_less_coffee_and_water_valid():
     t = ValidSerial()
     write_buffer = init_seq_coffee_brew(t)
     write_buffer += encode_str("WE:00D6,0011")
     t.read_buffer += encode_str("ok:")
     write_buffer += encode_str("WE:013C,0012")
     t.read_buffer += encode_str("ok:")
-    write_buffer += trailing_seq_coffee_brew(t)
+    write_buffer += encode_str(JuraCommand.BUTTON_4)
+    t.read_buffer += encode_str("ok:")
 
     def callback():
         assert False
 
     p = JuraProtocol(t, unexpected_msg_callback=lambda c: callback())
     m = CoffeeMaker(p)
-    await m.brew_coffee(CoffeeMaker.coffee_bean_param[1] - 2,
-                        CoffeeMaker.water_volume_param[1] - CoffeeMaker.water_volume_param[3] * 2, lambda v: None)
+    m.brew_coffee(CoffeeMaker.coffee_bean_param[1] - 2,
+                  CoffeeMaker.water_volume_param[1] - CoffeeMaker.water_volume_param[3] * 2)
 
     assert t.read_index == len(t.read_buffer)
     assert t.write_buffer == write_buffer
